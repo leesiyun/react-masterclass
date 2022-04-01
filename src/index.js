8de9ccff9d6ffc523930244958339fd1c31d2af2
Siyun Lee <siyun1221@gmail.com>
2022-04-01 13:55:53 +0900

    #2.1 Our First Styled Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("app");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
