4b3dee06f7492fb8f5fa54a99e444321cceec2fa
Siyun Lee <siyun1221@gmail.com>
2022-04-04 14:29:39 +0900

    #2.2 Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.backgroundColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 const Text = styled.span`
@@ -23,10 +21,8 @@ const Text = styled.span`
 const App = () => {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box backgroundColor="teal" />
+      <Circle backgroundColor="tomato" />
     </Father>
   );
 };
